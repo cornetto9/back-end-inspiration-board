fccdfc2a893e10ce2876935a1b160e36bf0def0d
rad-hi7 <53797949+rad-hi7@users.noreply.github.com>
2024-12-27 02:20:29 -0500

    Start unit tests for the card model

---
+++ tests/tests_card.py
@@ -0,0 +1,19 @@
+from app.models.card import Card
+from app.db import db
+import pytest
+
+def test_post_card_to_board(client, one_board):
+    # Act
+    response = client.post("/boards/1/cards", json={
+        "message": "All you can do is try"
+    })
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 201
+    assert "id" in response_body
+    assert "card_ids" in response_body
+    assert response_body == {
+        "id": 1,
+        "message": "All you can do is try"
+    }
