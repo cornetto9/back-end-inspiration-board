b89409bc0c134e4a5875af4edee7eb0cbe9f21ed
rad-hi7 <53797949+rad-hi7@users.noreply.github.com>
2025-01-03 14:23:50 -0500

    Remove optional

--- app/models/board.py
+++ app/models/board.py
@@ -1,12 +1,12 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ..db import db
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING
 
 class Board(db.Model):
     board_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     owner: Mapped[str]
-    cards: Mapped[Optional[list['Card']]] = relationship('Card', back_populates='board')
+    cards: Mapped[list['Card']] = relationship('Card', back_populates='board')
 
     def to_dict(self):
         return {
